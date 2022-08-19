16f63da0bf2fbe583edd1754493d3c2c429022bd
statorjso <statorjso@gmail.com>
2022-08-19 12:01:07 -0400

    more beginning changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {authenticated: false};
+  }
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Justin's React app.
-        </p>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" />
-        <label for="password">Password:</label>
-        <input type="text" id="password" name="password" />
-      </header>
-    </div>
-  );
+  render() {
+    if (!this.state.authenticated)
+    {
+      return ( <Login /> );
+    }
+  } 
+}
+
+class Login extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {username: '', password: ''};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to Justin's React app.
+          </p>
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" />
+          <label for="password">Password:</label>
+          <input type="text" id="password" name="password" />
+        </header>
+      </div>
+      );
+  }
+}
+
+class Content extends React.Component{
+  constructor(props) {
+    super(props);
+    // this.state = {username: '', password: ''};
+
+    // This binding is necessary to make `this` work in the callback
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handleClick() {
+  //   this.setState(prevState => ({
+  //     isToggleOn: !prevState.isToggleOn
+  //   }));
+  // }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            Successful login.
+          </p>
+          Yay
+        </header>
+      </div>
+      );
+  }
 }
 
 export default App;
